984a6f404854224b319a32e64317d2e563fd0955
nonato-barroso91 <nonatobarroso14@gmail.com>
2024-09-21 17:38:03 -0300

    feat
    codigos novos

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class SumTwoNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("A soma é: " + sum);
     }
 }
